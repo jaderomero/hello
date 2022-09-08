aa876b1c85c168ac241dccc1b35bceb66b9572b4
Jade Romero <jromero16@huskers.unl.edu>
2022-09-08 05:16:32 +0000

    Thu, Sep 8, 2022, 5:16 AM +00:00

---
+++ hello.c
@@ -0,0 +1,13 @@
+/** Jade Romero
+ * 9/8/2022
+ * A redo for fix it week for hack one
+ * */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jade Romero\n");
+
+}
+
